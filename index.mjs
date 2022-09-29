4f09e2955f346ad032d4025bb027f82cab7f93f9
Nicolas Galdino <nicolasesmael1998@gmail.com>
2022-09-29 17:07:49 -0300

    Ok, vacilei. Estrutura de pasta cirrigda. Haha

--- index.mjs
+++ index.mjs
@@ -1,5 +1,5 @@
 import chalk from 'chalk';
-import operation from "./operation.mjs";;
+import operation from "./project_functions/operation.mjs";
 
 console.log(chalk.bgGreen.black("Obrigado por escolher nosso banco."));
 console.log(chalk.green("Siga as intruções abaixo."));
